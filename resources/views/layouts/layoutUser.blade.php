aa2ef04cbcbf7a5943099ef71cadac6e9df593b8
Tram Huynh <huynhthidieutram97@gmail.com>
2019-02-14 14:53:25 +0700

    abc

--- resources/views/layouts/layoutUser.blade.php
+++ resources/views/layouts/layoutUser.blade.php
@@ -255,47 +255,47 @@
 <script src="{{ asset('public/assets/js/main.js') }}"></script>
 
 <!-- Nhat's js-->
-<script src="{{ asset('public/js/myCustom.js')}}"></script>
+{{--<script src="{{ asset('public/js/myCustom.js')}}"></script>--}}
 
-<script>
-    var APP_URL = {!! json_encode(url('/')) !!}
-    $(document).ready(function () {
+{{--<script>--}}
+    {{--var APP_URL = {!! json_encode(url('/')) !!}--}}
+    {{--$(document).ready(function () {--}}
 
-        var json= $.ajax({
-            type: 'get',
-            dataType: "json",
-            url: APP_URL+'/get-notification',
-            success: function (data) {
-                $('#badge').text(data.count+'');
-                if (data.isExpired != null) {
-                    $('#li-room').show();
-                    var time = new Date(data.expire.date);
-                    var month= ("0" + (time.getMonth() + 1)).slice(-2);
-                    var year = time.getFullYear();
-                    var day= ("0" + (time.getDay() )).slice(-2);
-                    if(data.isExpired == '0')
-                        $('#noti-room').text("Bạn đã hết hạn phòng, cần gia hạn ngay. Hạn cuối là: "+ day+'/'+month+'/'+year);
-                    if(data.isExpired == '1')
-                        $('#noti-room').text("Bạn sắp hết hạn phòng, cần gia hạn ngay. Hạn cuối là: "+ day+'/'+month+'/'+year);
-                }
-                if(data.money){
-                    $('#li-bill').show();
-                    var now = new Date();
-                    var month= ("0" + (now.getMonth() + 1)).slice(-2);
-                    var year =now.getFullYear();
-                    $('#noti-bill').text('Tiền điện tháng này là: '+ data.money.total+' VNĐ bạn vẫn chưa nộp. Hạn cuối là 25/'+month+'/'+year);
-                }
+        {{--var json= $.ajax({--}}
+            {{--type: 'get',--}}
+            {{--dataType: "json",--}}
+            {{--url: APP_URL+'/get-notification',--}}
+            {{--success: function (data) {--}}
+                {{--$('#badge').text(data.count+'');--}}
+                {{--if (data.isExpired != null) {--}}
+                    {{--$('#li-room').show();--}}
+                    {{--var time = new Date(data.expire.date);--}}
+                    {{--var month= ("0" + (time.getMonth() + 1)).slice(-2);--}}
+                    {{--var year = time.getFullYear();--}}
+                    {{--var day= ("0" + (time.getDay() )).slice(-2);--}}
+                    {{--if(data.isExpired == '0')--}}
+                        {{--$('#noti-room').text("Bạn đã hết hạn phòng, cần gia hạn ngay. Hạn cuối là: "+ day+'/'+month+'/'+year);--}}
+                    {{--if(data.isExpired == '1')--}}
+                        {{--$('#noti-room').text("Bạn sắp hết hạn phòng, cần gia hạn ngay. Hạn cuối là: "+ day+'/'+month+'/'+year);--}}
+                {{--}--}}
+                {{--if(data.money){--}}
+                    {{--$('#li-bill').show();--}}
+                    {{--var now = new Date();--}}
+                    {{--var month= ("0" + (now.getMonth() + 1)).slice(-2);--}}
+                    {{--var year =now.getFullYear();--}}
+                    {{--$('#noti-bill').text('Tiền điện tháng này là: '+ data.money.total+' VNĐ bạn vẫn chưa nộp. Hạn cuối là 25/'+month+'/'+year);--}}
+                {{--}--}}
 
 
-            }
-        });
+            {{--}--}}
+        {{--});--}}
 
-    });
+    {{--});--}}
 
-</script>
+{{--</script>--}}
 <!-- common js-->
 <script src="{{ asset('public/js/common/common.js')}}"></script>
 
-<script src="{{ asset('public/js/Client/home.js')}}"></script>
+{{--<script src="{{ asset('public/js/Client/home.js')}}"></script>--}}
 </body>
 </html>
